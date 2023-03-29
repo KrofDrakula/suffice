874c1fe305b57efeaa9d57f40a1fd6b9ab42e04b
Klemen Slavič <krof.drakula@gmail.com>
2023-03-29 13:25:24 +0200

    Fix assertions

--- packages/interpreter/src/grammar/index.test.ts
+++ packages/interpreter/src/grammar/index.test.ts
@@ -13,66 +13,77 @@ describe("grammar", () => {
     expect(parse("123")).toEqual({
       type: "number",
       value: 123,
+      location: { start: 0, end: 3 },
     } satisfies NumberValue);
     expect(parse("1.23")).toEqual({
       type: "number",
       value: 1.23,
+      location: { start: 0, end: 4 },
     } satisfies NumberValue);
     expect(parse("1.2e3")).toEqual({
       type: "number",
       value: 1200,
+      location: { start: 0, end: 5 },
     } satisfies NumberValue);
     expect(parse("123e-2")).toEqual({
       type: "number",
       value: 1.23,
+      location: { start: 0, end: 6 },
     } satisfies NumberValue);
   });
 
   it("parses string literals", () => {
-    expect(parse(`""`)).toEqual({
+    expect(parse(`=""`)).toEqual({
       type: "string",
       value: "",
+      location: { start: 1, end: 3 },
     } satisfies StringValue);
-    expect(parse(`"123"`)).toEqual({
+    expect(parse(`="123"`)).toEqual({
       type: "string",
       value: "123",
+      location: { start: 1, end: 6 },
     } satisfies StringValue);
-    expect(parse(`"ab\\"c"`)).toEqual({
+    expect(parse(`="ab\\"c"`)).toEqual({
       type: "string",
       value: `ab"c`,
+      location: { start: 1, end: 8 },
     } satisfies StringValue);
   });
 
   it("parses booleans", () => {
-    expect(parse("true")).toEqual({
+    expect(parse("=true")).toEqual({
       type: "boolean",
       value: true,
+      location: { start: 1, end: 5 },
     } satisfies BooleanValue);
-    expect(parse("false")).toEqual({
+    expect(parse("=false")).toEqual({
       type: "boolean",
       value: false,
+      location: { start: 1, end: 6 },
     } satisfies BooleanValue);
   });
 
   it("parses cell references", () => {
-    expect(parse("A1")).toEqual({
+    expect(parse("=A1")).toEqual({
       type: "reference",
       column: 1,
       columnLock: false,
       row: 1,
       rowLock: false,
+      location: { start: 1, end: 3 },
     } satisfies CellReference);
-    expect(parse("$B$2")).toEqual({
+    expect(parse("=$B$2")).toEqual({
       type: "reference",
       column: 2,
       columnLock: true,
       row: 2,
       rowLock: true,
+      location: { start: 1, end: 5 },
     } satisfies CellReference);
   });
 
   it("parses cell ranges", () => {
-    expect(parse("A1:B2")).toEqual({
+    expect(parse("=A1:B2")).toEqual({
       type: "range",
       start: {
         type: "reference",
@@ -80,6 +91,7 @@ describe("grammar", () => {
         rowLock: false,
         column: 1,
         columnLock: false,
+        location: { start: 1, end: 3 },
       },
       end: {
         type: "reference",
@@ -87,7 +99,9 @@ describe("grammar", () => {
         rowLock: false,
         column: 2,
         columnLock: false,
+        location: { start: 4, end: 6 },
       },
+      location: { start: 1, end: 6 },
     } satisfies CellRange);
   });
 });
