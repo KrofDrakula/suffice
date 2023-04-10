a842eeba6d5764dcfedaa905c3b4b7eb10a8a289
Klemen Slavič <krof.drakula@gmail.com>
2023-04-10 14:42:50 +0200

    Rework dev example to be live editable

---
+++ packages/prism-langugage-def/src/interactive.ts
@@ -0,0 +1,10 @@
+const ta = document.body.querySelector("textarea")! as HTMLTextAreaElement;
+const styled = document.body.querySelector("#code")! as HTMLDivElement;
+
+ta.addEventListener("input", (ev) => {
+  const text = (ev.target as HTMLTextAreaElement).value;
+  styled.textContent = text;
+  Prism.highlightElement(styled);
+});
+
+styled.textContent = ta.value;
